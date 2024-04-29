9aa9f1c35dfe90b7125bd42e192b7a32d3ce99af
Igor Stojanovic <118001688+Dzigiii@users.noreply.github.com>
2024-04-29 18:11:16 +0200

    Update NaucnaKnjiga.java

--- oop/rokovi_resenja/2024/Jan1A/1/NaucnaKnjiga.java
+++ oop/rokovi_resenja/2024/Jan1A/1/NaucnaKnjiga.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class NaucnaKnjiga extends Knjiga implements Iznajmljivanje{
+public class NaucnaKnjiga extends Knjiga{
 
     String naucnaOblast;
 
